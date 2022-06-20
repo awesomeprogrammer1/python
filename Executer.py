72571c8640ac858e6d150f123b18105ef14eed4e
Awesome Programmer <fluffycatsavage@gmail.com>
2022-06-20 15:54:07 -0400

    Added 'papa'

--- Executer.py
+++ Executer.py
@@ -1,4 +1,4 @@
-name = input('Hello! I am waiter_bot. What is your name? ')
+name = input('Hello! I am a papa waiter_bot. What is your name? ')
 eat = input('Would you like to eat today,'+name + ' ' )
 if (eat.lower() == 'of course')  or (eat.lower() == 'yes') or (eat.lower() == 'maybe') :
     print('Perfect! Heres a menu.')
