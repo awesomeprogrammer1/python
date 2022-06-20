26096278e89c4cae38417935266c41420da491ce
Awesome Programmer <fluffycatsavage@gmail.com>
2022-06-20 16:23:31 -0400

    add decrypt method

--- hello-world/Caesar ciphers.py
+++ hello-world/Caesar ciphers.py
@@ -10,8 +10,29 @@ def encrypt(text,key):
             result+=letter
     return(result)
     
+def decrypt(text,key):
+    result = ''
+    for letter in text:
+        if letter in alphabet:
+            position=alphabet.find(letter)
+            new_position=position+key
+            original_position= position-key
+            result+=alphabet[original_position]
+        else:
+            result+=letter
+    return(result)
+
+
+
+
+
 if __name__ == "__main__":
-   my_text = 'Hedgie'
-   print(my_text)
-   my_encrypted_text = encrypt(my_text, 1)
+   my_original_text = 'Hedgie'
+   print(my_original_text)
+   my_encrypted_text = encrypt(my_original_text, 1)
    print(my_encrypted_text)
+   my_decrypted_text = decrypt(my_encrypted_text,1)
+   print(my_decrypted_text)
+
+
+
