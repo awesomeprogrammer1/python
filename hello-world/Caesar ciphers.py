24c80dcaa14b1d2b2a4e8e04ee9327f1e3195a42
Awesome Programmer <fluffycatsavage@gmail.com>
2022-06-21 12:05:20 -0400

    add variable alphabet_size

--- hello-world/Caesar ciphers.py
+++ hello-world/Caesar ciphers.py
@@ -1,4 +1,5 @@
-alphabet= 'abcdefghijklmnopqrstuvwxyz'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_size = 26
 
 scramble_amount = int(input('By how many spaces will each letter move? '))
 
@@ -8,8 +9,8 @@ def encrypt(text,key):
         if letter in alphabet:
             position=alphabet.find(letter)
             new_position=position+key
-            if new_position >= 26:
-                new_position=new_position-26
+            if new_position >= alphabet_size:
+                new_position=new_position-alphabet_size
             result+=alphabet[new_position]
         else:
             result+=letter
@@ -22,8 +23,8 @@ def decrypt(text,key):
             position=alphabet.find(letter)
             new_position=position+key
             original_position= position-key
-            if original_position >= 26:
-                original_position=original_position+26
+            if original_position >= alphabet_size:
+                original_position=original_position+alphabet_size
             result+=alphabet[original_position]
         else:
             result+=letter
@@ -33,9 +34,9 @@ def decrypt(text,key):
 if __name__ == "__main__":
    my_original_text = 'abcdefghijklmnopqrstuvwxyz'
    print(my_original_text)
-   my_encrypted_text = encrypt(my_original_text, scramble_amount-26)
+   my_encrypted_text = encrypt(my_original_text, scramble_amount-alphabet_size)
    print(my_encrypted_text)
-   my_decrypted_text = decrypt(my_encrypted_text, scramble_amount-26)
+   my_decrypted_text = decrypt(my_encrypted_text, scramble_amount-alphabet_size)
    print(my_decrypted_text)
 
 
