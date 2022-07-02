e2a13a9250341871ba2cc16e031f6bc15b06056f
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-02 11:04:32 -0400

    add programs for 2022-07-02

---
+++ code_school/20220702_ex1.py
@@ -0,0 +1,21 @@
+import time
+
+mail = "admin@admin.com"
+password = "qwerty12345"
+while True:
+    for i in range(5):
+        user_mail = input("What is your email? ")
+        user_password = input("What is your password? ")
+        if user_mail == mail:
+            if user_password == password:
+                print("Welcome")
+                exit()
+            else:
+                print("Error Code 278; Password incorrect")
+        else:
+            print("Error Code 421; Mail incorrect")
+    print("Account locked for ten seconds")
+    for i in range(1, 11):
+        print(i)
+        time.sleep(1)
+    print("Account Unlocked")
---
+++ code_school/20220702_ex2.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a Number "))
+if 1 < number < 50:
+    print(number * 3)
+elif 50 <= number < 100:
+    print(number * 2)
+else:
+    print(number)
