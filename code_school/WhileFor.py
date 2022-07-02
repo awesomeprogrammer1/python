9b3bfa67475326afe123957c2b41e3663be577c0
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-02 09:54:46 -0400

    Update WhileFor

--- code_school/WhileFor.py
+++ code_school/WhileFor.py
@@ -1,8 +1,11 @@
-name = input('Hello! What is your name?')
+name = input("Hello! What is your name? ")
 for i in range(5):
-    age = int(input('How old are you?'))
+    age = int(input("How old are you? "))
     if age == 12:
-        print('Correct again', name)
+        print("Correct again", name)
         break
     else:
-        print('Wrong. Try again!')
+        if i < 4:
+            print("Wrong. Try again!")
+        else:
+            print("You are out of guesses, better luck next time!")
