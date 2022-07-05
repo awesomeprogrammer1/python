e9f4f8d6375b50b723aea6da754b90e0fd4381df
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-05 12:21:40 -0400

    assignment 2 complete

--- code_school/lists.py
+++ code_school/lists.py
@@ -1,10 +1,9 @@
 from easygui import *
 
-# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # assignment 1: print all numbers that are less than 5
 # assignment 2: print the sum of the elements less than 10 and bigger than 30
 # assignment 3: learn about averages and print all averages in the ray
-
 
 
 # list = [1,2,3,4,5]
@@ -40,15 +39,44 @@ from easygui import *
 #     msgbox('Welcome')
 # else:
 #     msgbox('sorry, not today')
- 
-list = [1,2,3,4,5,6,7,8,9,10]
+
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # for i in range(1, 10, 2):
 #     list[i] = list[i] * 2
-# print(list)   
+# print(list)
 
-for i in list:
-    if i<3 or i>7:
-        print(i)
+# for i in list:
+#     if i < 3 or i > 7:
+#         print(i)
+
+# assignment 1
+
+# for i in a:
+#     if i < 5:
+#         print(i)
+
+# assignment 2
 
 
+# for i in a:
+#     if i < 10 or i > 30:
+#         print(i)
+# newList = a[:6] + a[8:]
+# print(newList)
+# newList[6:7]
+# newerList = newList[6:9]
+# print(newerList)
+# newestList = newList[1:6]
+# print(newestList)
+# sum1 = 0
+# for i in newerList:
+#     sum1 += i
+# average = sum1 / len(newerList)
+# print(sum1, average)
+
+# sum2 = 0
+# for i in newestList:
+#     sum2 += i
+# average = sum2 / len(newestList)
+# print(sum2, average)
