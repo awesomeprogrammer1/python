de00a3e95639c0fd91e2e3dcca0c878b03e1754a
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-05 15:38:35 -0400

    update comments

--- code_school/lists.py
+++ code_school/lists.py
@@ -1,51 +1,54 @@
 from easygui import *
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# assignment 1: print all numbers that are less than 5
-# assignment 2: print the sum of the elements less than 10 and bigger than 30
-# assignment 3: learn about averages and print all averages in the ray
+list = [1, 2, 3, 4, 5]
+
+list.append(6)  # operation: adds 6 to the list
+print(list)
+
+list.insert(3, "Alex")  # operation: inserts "Alex" at the index 3
+print(list)
+
+list.remove(3)  # operation: Removes the integer "3" from the list
+print(list)
+
+list = [1, 2, 3, 4, 5]
+# removes the middle number, since we count from 5 if its a negative number, it removes the integer  "4"
+list.pop(-2)
+print(list)
+
+# operation: inputs "Andrew" instead of the 2nd index, so it would change 5 for "Andrew"
+list[3] = "Andrew"
+print(list)
+
+# operation: inserts the integer "0" 100 times by running a for statement which runs 100 times
+list = [1, 1]
+for i in range(100):
+    list.insert(1, 0)
+print(list)
+
+# prints all the even numbers in the list by skip counting by two
+list = []
+for i in range(2, 101, 2):
+    list.append(i)
+print(list)
 
 
-# list = [1,2,3,4,5]
+# list that checks an input and executes a piece of code depending on the data
+list = ["Alex", "Sergey", "Andrew"]
+name = enterbox("Hello! What is your name? ")
+if name in list:
+    msgbox("Welcome")
+else:
+    msgbox("sorry, not today")
 
-# list.append(6) # operation: adds 6 to the list
-# print(list)
+# operation: multiplies every second number in the list by two repeatedly using a for statement
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in range(1, 10, 2):
+    list[i] = list[i] * 2
+print(list)
 
-# list.insert(3, "Alex") # operation: inserts "Alex" into the index 3 and shifts the character already there
-# print(list)
-
-# list.remove(3) # operation: Removes the integer "3" from the list
-# print(list)
-
-# list.pop(-2) # removes the middle number, since we count from 5 if its a negative number, it removes the integer  "3"
-# print(list)
-
-# list[3]= "Andrew" # operation: inputs "Andrew" instead of the 2nd index, so it would change 3 for "Andrew"
-# print(list)
-
-# list = [1, 1]
-# for i in range(100):
-#     list.insert(1, 0)
-# print(list)
-
-# list = []
-# for i in range(2, 101, 2):
-#     list.append(i)
-# print(list)
-
-# list = ["Alex", "Sergey", "Andrew"]
-# name = enterbox('Hello! What is your name? ')
-# if name in list:
-#     msgbox('Welcome')
-# else:
-#     msgbox('sorry, not today')
-
-# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-# for i in range(1, 10, 2):
-#     list[i] = list[i] * 2
-# print(list)
-
-# for i in list:
-#     if i < 3 or i > 7:
-#         print(i)
+# checks the condition that if i is less than three of greater than 7. If the condition holds true, it prints the integer
+for i in list:
+    if i < 3 or i > 7:
+        print(i)
+print(list)
