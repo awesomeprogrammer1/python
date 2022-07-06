5428cb3b6cae79a0cd0d418da11ff8698853416c
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-06 12:50:26 -0400

    tidy things up

--- code_school/20220710_ex1_ex2_ex3.py
+++ code_school/20220710_ex1_ex2_ex3.py
@@ -1,6 +1,4 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-from typing import List
-from easygui import *
 
 # assignment 1: print all numbers that are less than 5
 # assignment 2: print the sum of the elements less than 10 and bigger than 30
@@ -8,6 +6,7 @@ from easygui import *
 
 # assignment 1
 
+print("Elements less than 5:")
 for i in a:
     if i < 5:
         print(i)
@@ -18,7 +17,7 @@ filtered_list = []
 for i in a:
     if i < 10 or i > 30:
         filtered_list.append(i)
-print('Filtered list:')        
+print("Filtered list:")
 print(filtered_list)
 
 sum_filtered_list = sum(filtered_list)
