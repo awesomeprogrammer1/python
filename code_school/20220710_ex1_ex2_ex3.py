1cf429c7254547f1bc65125d3b53f3d80610c8f3
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-06 12:30:04 -0400

    make changes

--- code_school/20220710_ex1_ex2_ex3.py
+++ code_school/20220710_ex1_ex2_ex3.py
@@ -1,4 +1,5 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+from typing import List
 from easygui import *
 
 # assignment 1: print all numbers that are less than 5
@@ -7,64 +8,41 @@ from easygui import *
 
 # assignment 1
 
-# for i in a:
-#     if i < 5:
-#         print(i)
+for i in a:
+    if i < 5:
+        print(i)
 
 # assignment 2
 
-newList = []
-newerList = []
+filteredList = []
+List = []
 for i in a:
     if i < 10 or i > 30:
-        newList.append(i)
-print(newList)
+        filteredList.append(i)
+print(filteredList)
 for i in a:
     if i < 10:
-        newList.remove(i)
-        newerList.append(i)
-print(newerList)
-print(newList)
+        filteredList.remove(i)
+        List.append(i)
+print(List)
+print(filteredList)
 
 
-# b = a[:6] + a[8:]
-# print(b)
-# b[6:7]
-# c = b[6:9]
-# print(c)
-# d = b[1:6]
-# print(d)
 
 
-sum1 = sum(newerList)
+
+sum1 = sum(List)
 print(sum1)
 
-sum2 = sum(newList)
+sum2 = sum(filteredList)
 print(sum2)
 
-
-# sum1 = 0
-# for i in b:
-#     sum1 += i
-
-# print(sum1)
-
-
-# sum1 += i
-# average = sum1 / len(c)
-# print(sum1, average)
-
-# sum2 = 0
-
-# sum2 += i
-# average = sum2 / len(d)
-# print(sum2, average)
 
 
 # assignment 3
 
-# sum = 0
-# for i in a:
-#     sum += i
-# average = sum / len(a)
-# print(sum, average)
+sum = 0
+for i in a:
+    sum += i
+average = sum / len(a)
+print(sum, average)
