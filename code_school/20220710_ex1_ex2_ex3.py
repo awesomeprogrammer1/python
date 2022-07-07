ac68ba4db46bf2276870c2b547190141c7c2c2ca
awesomeprogrammer1 <66175906+awesomeprogrammer1@users.noreply.github.com>
2022-07-07 10:49:33 -0400

    Homework for 2022-07-09 (#5)
    # what
    
    Homework for 2022-07-09
    
    - assignment 1: print all numbers that are less than 5
    - assignment 2: print the sum of the elements less than 10 and bigger than 30
    - assignment 3: learn about averages and print the average of the array (list) 
    
    # why
    
    Practice concepts learned

---
+++ code_school/20220710_ex1_ex2_ex3.py
@@ -0,0 +1,29 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+# assignment 1: print all numbers that are less than 5
+# assignment 2: print the sum of the elements less than 10 and bigger than 30
+# assignment 3: learn about averages and print the average of elements in the list
+
+# assignment 1
+
+print("Elements less than 5:")
+for i in a:
+    if i < 5:
+        print(i)
+
+# assignment 2
+
+filtered_list = []
+for i in a:
+    if i < 10 or i > 30:
+        filtered_list.append(i)
+print("Filtered list:")
+print(filtered_list)
+
+sum_filtered_list = sum(filtered_list)
+print(f"sum_filtered_list={sum_filtered_list}")
+
+# assignment 3
+
+average_filtered_list = sum_filtered_list / len(filtered_list)
+print(f"average_filtered_list={average_filtered_list}")
--- code_school/lists.py
+++ code_school/lists.py
@@ -1,54 +1,54 @@
 from easygui import *
 
-# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# assignment 1: print all numbers that are less than 5
-# assignment 2: print the sum of the elements less than 10 and bigger than 30
-# assignment 3: learn about averages and print all averages in the ray
+list = [1, 2, 3, 4, 5]
+
+list.append(6)  # operation: adds 6 to the end of the list
+print(list)
+
+list.insert(3, "Alex")  # operation: inserts "Alex" at the index 3
+print(list)
+
+list.remove(3)  # operation: Removes the integer "3" from the list
+print(list)
+
+list = [1, 2, 3, 4, 5]
+# removes the middle number, since we count from 5 if its a negative number, it removes the integer  "4"
+list.pop(-2)
+print(list)
+
+# operation: inputs "Andrew" instead of the 2nd index, so it would change 5 for "Andrew"
+list[3] = "Andrew"
+print(list)
+
+# inserts 100 zeros between the first and last elements of the list
+list = [1, 1]
+for i in range(100):
+    list.insert(1, 0)
+print(list)
+
+# prints every even number from 2 to one hundred
+list = []
+for i in range(2, 101, 2):
+    list.append(i)
+print(list)
 
 
+# asks for a name. if the name is on the list, the person is admitted. If not, they are rejected
+list = ["Alex", "Sergey", "Andrew"]
+name = enterbox("Hello! What is your name? ")
+if name in list:
+    msgbox("Welcome")
+else:
+    msgbox("sorry, not today")
 
-# list = [1,2,3,4,5]
+# we are multiplying every even number by two
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in range(1, 10, 2):
+    list[i] = list[i] * 2
+print(list)
 
-# list.append(6) # operation: adds 6 to the list
-# print(list)
-
-# list.insert(3, "Alex") # operation: changes the integer '3' with "Alex"
-# print(list)
-
-# list.remove(3) # operation: Removes the integer "3" from the list
-# print(list)
-
-# list.pop(-2) # removes the middle number, since we count from 5 if its a negative number, it removes the integer  "3"
-# print(list)
-
-# list[3]= "Andrew" # operation: inputs "Andrew" instead of the 2nd index, so it would change 3 for "Andrew"
-# print(list)
-
-# list = [1, 1]
-# for i in range(100):
-#     list.insert(1, 0)
-# print(list)
-
-# list = []
-# for i in range(2, 101, 2):
-#     list.append(i)
-# print(list)
-
-# list = ["Alex", "Sergey", "Andrew"]
-# name = enterbox('Hello! What is your name? ')
-# if name in list:
-#     msgbox('Welcome')
-# else:
-#     msgbox('sorry, not today')
- 
-list = [1,2,3,4,5,6,7,8,9,10]
-
-# for i in range(1, 10, 2):
-#     list[i] = list[i] * 2
-# print(list)   
-
+# prints elements of the list that are less than 3 and/or greater than 7
 for i in list:
-    if i<3 or i>7:
+    if i < 3 or i > 7:
         print(i)
-
-
+print(list)
