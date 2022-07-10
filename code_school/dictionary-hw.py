67be5228e9ce8e9fcc0d731a3fed77de415be2a7
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-10 09:39:02 -0400

    uncomment code and tidy things up

---
+++ code_school/dictionary-hw.py
@@ -0,0 +1,5 @@
+# assignment 1: Explain what all the functions do
+# assignment 2: make a dictionary of 5 things you like and don't like and make a guessing game
+# assignment 3: index() and rindex() in lists 
+# assignment 4: learn keys(), items() and values() in dictionary
+
--- code_school/dictionary.py
+++ code_school/dictionary.py
@@ -1,45 +1,57 @@
-# dict = {
-#    "Hello": "Привет",
-#     "Car": "Машина",
-#     "Dog": "Собака"
-# }
+dict = {
+   "Hello": "Привет",
+    "Car": "Машина",
+    "Dog": "Собака"
+}
 
-# assignment 1: Explain what all the functions do
-# assignment 2: make a dictionary of 5 things you like and don't like and make a guessing game
-# assignment 3: index and rindex in lists 
-# assignment 4: learn keys items and values in dictionary
+print(dict["Hello"]) #
+print(dict.get("Hello")) #
+print(dict.setdefault("Hello")) #
+print(dict)
 
+input()
 
+###################################################
 
+dict = {
+   "Hello": "Привет",
+    "Car": "Машина",
+    "Dog": "Собака"
+}
 
-# print(dict["Hello"]) #
-# print(dict.get("Hello")) #
-# print(dict.setdefault("Hello")) #
-# print(dict)
+dict["Cat"] = "kot" # dict[key] = value
+print(dict)
 
+dict.pop("Car") #
+print(dict)
 
-# dict["Cat"] = "kot" # dict[key] = value
-# print(dict)
+input()
 
-# dict.pop("Car") #
-# print(dict)
+###################################################
 
-# dict = {}
+dict = {}
 
-# for i in range(1, 101):
-#     dict[i] = i * 2
-# print(dict)
+for i in range(1, 101):
+    dict[i] = i * 2
+print(dict)
 
+input()
 
-# list = [1, 2, 5, 473, 987, 560, 35]
-# dict = {}
-# for i in list:
-#     dict[i] = i/2
+###################################################
 
-# for i in list:
-#     if i % 2 == 0:
-#         dict[i] = i * 2
-# print(dict)
+list = [1, 2, 5, 473, 987, 560, 35]
+dict = {}
+for i in list:
+    dict[i] = i/2
+
+for i in list:
+    if i % 2 == 0:
+        dict[i] = i * 2
+print(dict)
+
+input()
+
+###################################################
 
 dict = {}
 
