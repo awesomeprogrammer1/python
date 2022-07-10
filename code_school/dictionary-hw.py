8d8731f2860efe40e2a4f7466ebfad501ffac58f
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-10 11:52:24 -0400

    resolve problems

--- code_school/dictionary-hw.py
+++ code_school/dictionary-hw.py
@@ -9,28 +9,35 @@
 # Assignment 2:
 
 
+
+
 dict = {
-    "Ice Cream": "Broccoli",
-    "Hockey": "Figure Skating",
-    "Being with friends": "Sitting at home",
-    "Staying up Late": "Waking up early",
-    "Fruits": "Vegetables",
+    "Ice Cream": "Like",
+    "broccoli": "don't like",
+    "hockey": "Like",
+    "figure skating": "don't like",
+    "staying up late": "Like",
+    "waking up early": "don't like",
+    "fruits" : "Like",
+    "vegetables" : "don't like"
+
 }
 print(dict)
-thisdict = dict.fromkeys(dict, "Like")
-thatdict = dict.values()
+# thisdict = dict.fromkeys(dict, "Like")
+# thatdict = dict.fromkeys(dict, "don't like")
+
+testdict = dict.values()
+print(testdict)
 
 guess = input("Guess what I enjoy! ")
 
 
-if guess in thisdict:
+if dict.setdefault(guess) == "Like":
     print("Correct! You Know Me Very Well")
     exit()
-else:
-    print("I either do not like that thing, or I do not have an opinion on it. Try to be more specific next time!")
-if guess in thatdict:
+if dict.setdefault(guess) == "don't like":
     print("I do not like that thing")
     exit()
 else:
-    print("I do not have an opinion on that. Try to be more specific! ")
-    exit()
+    print('I do not have an opinion on that thing, or you may have had a typo. Try again, there may be a problem with capital letters at the start')
+
