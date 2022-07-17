d102cb120414f29d9dcdf1b289bf59dc7135a1e2
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-17 12:19:22 -0400

    update examples and progress assignment 1

---
+++ code_school/20220717.py
@@ -0,0 +1,21 @@
+dict = {}
+word1 = "abcde"
+word2 = "12345"
+for i in range(len(word1)):
+    dict[word1[i]] = word2[i]
+print(dict)
+word3 = "hedge"
+# for i in range(len(word3)):
+print(word2)
+
+
+str1 = "hedge"
+str2 = "hog"
+str1 += str2
+print(str1)
+
+
+n1 = 5
+n2 = 2
+n1 += n2
+print(n1)  # has to display 7
