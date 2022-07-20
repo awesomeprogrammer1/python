e45f738d7b05cb5e5432b4f3e5d0f40c2a609be4
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-20 19:43:48 -0400

    save ver

--- code_school/20220723 HW.py
+++ code_school/20220723 HW.py
@@ -2,17 +2,14 @@ dict = {}
 
 sentence1_words = input("Enter a sentence: ").split()
 sentence2_words = input("Enter a sentence with the same amount of words as the first one: ").split()
-list = []
 
 
 if len(sentence1_words) != len(sentence2_words):
-    print("Error")
+    print('Error')
     exit()
 for i in range(len(sentence1_words)):
-    list.append(sentence1_words[i])
     if sentence1_words[i] in dict.keys():
-        
-        dict[sentence1_words[i]] = [dict[list]]
-    else:
+        dict[sentence1_words[i]] =  dict[sentence1_words[i]] + ", " + sentence2_words[i]
+    else:    
         dict[sentence1_words[i]] = sentence2_words[i]
-print(dict)
+print(dict) 
