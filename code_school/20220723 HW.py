7ea6ef7eda01fe781f688ffd7f83b8da7dde9db3
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-23 10:28:01 -0400

    finish hw

--- code_school/20220723 HW.py
+++ code_school/20220723 HW.py
@@ -1,34 +1,48 @@
 # assignment 1
+# castle = [1, ["c"], 543, "P", ["e"], ["n", ["r"]], "i", [[["s"]]]]
+# a = "" + castle[3] + castle[]
 
 
-dict = {}
 
-sentence1_words = input("Enter a sentence: ").split()  # ["1 word", "2 word", "3 word"]
-sentence2_words = input(
-    "Enter a sentence with the same amount of words as the first one: "
-).split()
+# dict = {}
+
+# sentence1_words = input("Enter a sentence: ").split()  # ["1 word", "2 word", "3 word"]
+# sentence2_words = input(
+#     "Enter a sentence with the same amount of words as the first one: "
+# ).split()
 
 
-if len(sentence1_words) != len(sentence2_words):
-    print("Error")
-else:
-    for i in range(len(sentence1_words)):
-        # if sentence1_words[i] in dict.keys():
-        #     dict[sentence1_words[i]] =  dict[sentence1_words[i]] + ", " + sentence2_words[i]
-        #
+# if len(sentence1_words) != len(sentence2_words):
+#     print("Error")
+# else:
+#     for i in range(len(sentence1_words)):
+  
+#         # if sentence1_words[i] in dict.keys():
+#         #     dict[sentence1_words[i]] =  dict[sentence1_words[i]] + ", " + sentence2_words[i]
+#         #
 
-        # list = []
-        # list.append(sentence2_words[i])
-        # dict[sentence1_words[i]] = list
+#         # list = []
+#         # list.append(sentence2_words[i])
+#         # dict[sentence1_words[i]] = list
 
-        if sentence1_words[i] in dict.keys():
-            list = []
-            list.append(dict[sentence1_words[i]])
-            list.append(sentence2_words[i])
-            dict[sentence1_words[i]] = list
-        else:
-            dict[sentence1_words[i]] = sentence2_words[i]
+#         # conditional operator checks if the word in the sentence is repeating
+#         if sentence1_words[i] in dict.keys(): 
+#             # create an empty list
+#             list = []
+#             # adding the value in the first element that the word appears
+#             list.append(dict[sentence1_words[i]])
+#             # adding the value in the second element that the word appears in
+#             list.append(sentence2_words[i])
+#             # adds the key 'dad' back under the values in the list
+#             dict[sentence1_words[i]] = list
+#         else:
+#             # adds the element to the dictionary
+#             dict[sentence1_words[i]] = sentence2_words[i]
 
-    print(dict)
+#     print(dict)
 
+# list = [['love', 'very'], 'too']
+# print(list[1])
+# print(list[0])
+# print(list[0][0])
 #
