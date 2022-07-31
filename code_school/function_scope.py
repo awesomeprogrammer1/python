0d724f4180b72ec231cbf37db06fbc5b085bae2c
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-31 17:03:43 -0400

    make slight progress

--- code_school/function_scope.py
+++ code_school/function_scope.py
@@ -6,19 +6,17 @@
 # sun(time), time: "06:00", "11:30",
 # return an angle-str  ,
 # "06:00"-0, "12:00"- 90, "18:00"-180,
+
+
 dict = {}
+list = []
 
 
-def sun(hours: int, minutes: int) -> str:
+def sun2(hours: int, minutes: int) -> str:
     if hours > 18 or 6 > hours:
         return "The Sun Is Down"
     elif minutes >= 60 or hours >= 24:
         return "Error, Invalid Input"
-    if minutes == 00:
-        dict[hours] = '00'
-        angle_by_hour = (hours - 6) * 15
-        angle_by_minutes = (minutes) * 0.25
-        return f"Time = {dict}, Angle Of The Sun = {angle_by_hour+angle_by_minutes}"
     else:
         dict[hours] = minutes
         angle_by_hour = (hours - 6) * 15
@@ -26,4 +24,11 @@ def sun(hours: int, minutes: int) -> str:
         return f"Time = {dict}, Angle Of The Sun = {angle_by_hour+angle_by_minutes}"
 
 
-print(sun(7, 00))
+def sun(time: str) -> str:
+    time = input("Enter a time using the military format and a comma (Example: 12:45)")
+    time.split(":")
+    list.append(time)
+
+
+
+print(sun2(7, 00))
