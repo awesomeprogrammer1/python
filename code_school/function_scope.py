c33906a20be5c672ac17b509127454a0b27e3cd4
Awesome Programmer <fluffycatsavage@gmail.com>
2022-07-31 12:01:06 -0400

    make backup

--- code_school/function_scope.py
+++ code_school/function_scope.py
@@ -10,15 +10,17 @@ dict = {}
 
 
 def sun(time: str) -> str:
-    angle = time / 3.75
+    subtracting_number = 600
     if time < 601 or time > 1800:
         return f"Time = {time}, Angle = {0}"
     else:
-        while True:
+        while time > subtracting_number:
             divisor = 4
-            angle = (time - 600) / divisor
-            return f"Time = {time}, Angle = {angle}"
+            angle = (time - subtracting_number) / divisor
+            subtracting_number += 100
+            if time - subtracting_number >
+            angle += 15
+        return f"Time = {time}, Angle = {angle}"
 
 
-
-print(sun(700))
+print(sun(1000))
