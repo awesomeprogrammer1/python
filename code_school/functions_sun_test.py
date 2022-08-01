5dede7d33cbd3013b00c597095ac677c2160838e
Sergei Ryabkov <sergei@ryabkov.com>
2022-08-01 04:53:11 -0400

    add tests for sun

---
+++ code_school/functions_sun_test.py
@@ -0,0 +1,25 @@
+import pytest
+from functions_sun import sun
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("7:15", "Time = 7:15, Angle Of The Sun = 18.75"),
+        ("2:00", "The Sun Is Down"),
+        ("18:01", "The Sun Is Down"),
+        ("6:00", "Time = 6:00, Angle Of The Sun = 0"),
+        ("6:01", "Time = 6:01, Angle Of The Sun = 0.25"),
+        ("17:59", "Time = 17:59, Angle Of The Sun = 179.75"),
+        ("18:01", "Time = 18:00, Angle Of The Sun = 180"),
+        ("12:00", "Time = 12:00, Angle Of The Sun = 90"),
+        ("-3:00", "Error, Invalid Input"),
+        ("-3:-24", "Error, Invalid Input"),
+        ("13:-24", "Error, Invalid Input"),
+        ("3:-24", "Error, Invalid Input"),
+        ("abc", "Error, Invalid Input"),
+        ("ab:cd", "Error, Invalid Input"),
+    ],
+)
+def test_sun(test_input: str, expected: str):
+    assert sun(test_input) == expected
