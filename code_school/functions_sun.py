bd2cae922fbf73f75a195bb54cc3977717ca2dfe
awesomeprogrammer1 <fluffycatsavage@gmail.com>
2022-08-03 16:49:04 -0400

    finishing homework

--- code_school/functions_sun.py
+++ code_school/functions_sun.py
@@ -20,7 +20,7 @@ def sun(time: str) -> str:
         hours_and_minutes = time.split(":")
         hours = int(hours_and_minutes[0])
         minutes = int(hours_and_minutes[1])
-    except ValueError:
+    except (TypeError, ValueError):
         return "Error, Invalid Input"
     if minutes <= -1 or hours <= -1:
         return "Error, Invalid Input"
