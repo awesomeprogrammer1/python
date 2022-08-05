c5efbd28e9724d74edcf6dafe0041c896c089e02
Awesome Programmer <fluffycatsavage@gmail.com>
2022-08-05 18:49:06 -0400

    finish assignment

--- code_school/calculator.py
+++ code_school/calculator.py
@@ -9,7 +9,7 @@ while True:
     calculator = buttonbox(
         "Choose to add, subtract, multiply, or divide",
         "Calculator",
-        ["add", "subtract", "multiply", "divide", "exit"],
+        ["add", "subtract", "multiply", "divide", "exponentiate", "exit"],
     )
     if calculator == "add":
         add_numbers = multenterbox(
@@ -31,15 +31,31 @@ while True:
         multiply_numbers = multenterbox(
             "Write Two Numbers To Multiply Together",
             "Calculator",
-            ["first number", "second number"])
+            ["first number", "second number"],
+        )
         multiplied_numbers = int(multiply_numbers[0]) * int(multiply_numbers[1])
         textbox(multiplied_numbers)
     elif calculator == "divide":
         divide_numbers = multenterbox(
             "Write Two Numbers To Divide",
             "Calculator",
-            ["first number", "second number"])
-        divided_numbers = int(divide_numbers[0]) / int(divide_numbers[1])
-        textbox(divided_numbers)
+            ["first number", "second number"],
+        )
+        if divide_numbers[0] or divide_numbers[1] == 0:
+            textbox("Error code 000: One or more numbers equivilant to zero")
+        else:
+            divided_numbers = int(divide_numbers[0]) / int(divide_numbers[1])
+            textbox(divided_numbers)
+    elif calculator == "exponentiate":
+        exponent_numbers = multenterbox(
+            "Write two numbers to perform an exponential equation",
+            "Calculator",
+            ["first number", "second number"],
+        )
+        if exponent_numbers[0] or exponent_numbers[1] == 0:
+            textbox("Error code 000: One or more numbers equivilant to zero")
+        else:
+            exponented_numbers = int(exponent_numbers[0]) ^ int(exponent_numbers[1])
+            textbox(exponented_numbers)
     elif calculator == "exit":
         exit()
