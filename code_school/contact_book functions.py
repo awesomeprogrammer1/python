c8179c3ba22d0af57acdcf5d30ccbf3d0906a2f6
Awesome Programmer <fluffycatsavage@gmail.com>
2022-08-13 09:54:38 -0400

    update code

--- code_school/contact_book functions.py
+++ code_school/contact_book functions.py
@@ -3,39 +3,37 @@ from easygui import *
 dict = {}
 
 
-def add_contact(interface_choice):
-    if interface_choice == "add":
-        try:
-            contact_info = multenterbox(
-                "Please Enter Contact Information ", "Add Contact", ["name", "phone number"]
+def add_contact():
+    try:
+        contact_info = multenterbox(
+            "Please Enter Contact Information ", "Add Contact", ["name", "phone number"]
         )
-            dict[contact_info[0]] = contact_info[1]
+        dict[contact_info[0]] = contact_info[1]
+        return textbox(dict)
+    except TypeError:
+        return textbox("Error: Please close to return to calculator")
+
+
+def remove_contact():
+    try:
+        remove_contact = enterbox(
+            "Please Enter The Name of the Contact ",
+            "Remove Contact",
+        )
+        if remove_contact not in dict:
+            return textbox("Error: Contact does not Exist, please try again ")
+        else:
+            dict.pop(remove_contact)
             return textbox(dict)
-        except TypeError:
-            return textbox("Error: Please close to return to calculator")
-
-def remove_contact(interface_choice):
-    if interface_choice == "remove":
-        try:
-            remove_contact = enterbox(
-                "Please Enter The Name of the Contact ",
-                "Remove Contact",
-            )
-            if remove_contact not in dict:
-                return textbox("Error: Contact does not Exist, please try again ")
-            else:
-                dict.pop(remove_contact)
-                return textbox(dict)
-        except TypeError:
-            return textbox("Error: Please close to return to calculator")
+    except TypeError:
+        return textbox("Error: Please close to return to calculator")
 
 
-def edit_contact(interface_choice):
-    if interface_choice == "edit":
-        contact_name = enterbox(
-            "Please Enter The Name Of The Contact You Would Like To Edit ",
-            "Edit Contact",
-        )
+def edit_contact():
+    contact_name = enterbox(
+        "Please Enter The Name Of The Contact You Would Like To Edit ",
+        "Edit Contact",
+    )
     if contact_name not in dict:
         return textbox("Error, Contact Does Not Exist.")
     else:
@@ -47,16 +45,15 @@ def edit_contact(interface_choice):
         return textbox(dict)
 
 
-def search_contact(interface_choice):
-    if interface_choice == "search":
-        search_contact = enterbox(
-            "Enter the Name of the Contact That You Would Like To Search",
-            "Search Contact",
-        )
-        if search_contact not in dict:
-            return textbox("Error: Contact Does Not Exist")
-        else:
-            return textbox(dict[search_contact], "Users Phone Number")
+def search_contact():
+    search_contact = enterbox(
+        "Enter the Name of the Contact That You Would Like To Search",
+        "Search Contact",
+    )
+    if search_contact not in dict:
+        return textbox("Error: Contact Does Not Exist")
+    else:
+        return textbox(dict[search_contact], "Users Phone Number")
 
 
 while True:
@@ -66,12 +63,12 @@ while True:
         ["add", "remove", "edit", "search", "exit"],
     )
     if interface == "add":
-        add_contact("add")
+        add_contact()
     elif interface == "remove":
-        remove_contact("remove")
+        remove_contact()
     elif interface == "edit":
-        edit_contact("edit")
+        edit_contact()
     elif interface == "search":
-        search_contact("search")
+        search_contact()
     elif interface == "exit":
         exit()
