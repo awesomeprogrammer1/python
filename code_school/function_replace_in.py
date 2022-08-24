4debd66425e08824db91fafe47c180f66c028ce0
Sergei Ryabkov <sergei@ryabkov.com>
2022-08-24 09:03:31 -0400

    add function_in_papa

--- code_school/function_replace_in.py
+++ code_school/function_replace_in.py
@@ -18,13 +18,30 @@ def replace(s: str, old_char: str, new_char: str) -> str:
     newest_s = new_s.replace(old_char, new_char)
     return newest_s
 
+# def replace2(s: str, find: str, replace: str) -> str:
+#     result = ""
+#     for i in range(len(s)):
+#         found_match = False
+#         if s[i] == find[i] and i + len(find) < len(s):
+#             for j in range(len(find)):
+#                 if s[i+j] != find[j]:
+#                     break
+#             else:
+#                 found_match = True
+#     split_list = s.split(find)
+#     return replace.join(split_list)
 
-def function_in(s: str, char: str) -> bool:
-    for element in s:
-        if element == char:
-            return True
+
+def function_in_papa(s: str, find: str) -> bool:
+    for i in range(len(s)):
+        if s[i] == find[0] and i + len(find) <= len(s):
+            for j in range(1, len(find)):
+                if s[i + j] != find[j]:
+                    break
+            else:
+                return True
     return False
 
 
 if __name__ == "__main__":
-    print(replace("Hello", "ll", "mm"))
+    print(function_in_papa("Andrew", "An"))
