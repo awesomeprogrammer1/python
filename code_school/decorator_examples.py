c6d05eb335869ed759ac37f1bada622e75eaad8c
Awesome Programmer <fluffycatsavage@gmail.com>
2022-08-24 14:01:14 -0400

    update decorators

--- code_school/decorator_examples.py
+++ code_school/decorator_examples.py
@@ -1,4 +1,4 @@
-def hello_decorator(func):
+def hello_decorator1(func):
 
     # inner1 is a Wrapper function in
     # which the argument is called
@@ -9,7 +9,8 @@ def hello_decorator(func):
         # calling the actual function now
         # inside the wrapper function.
         func()
-        print("This is after function execution")    
+        print("This is after function execution")
+
     return inner1
 
 
@@ -20,8 +21,61 @@ def function_to_be_used():
 
 # passing 'function_to_be_used' inside the
 # decorator to control its behaviour
-function_to_be_used = hello_decorator(function_to_be_used)
+function_to_be_used = hello_decorator1(function_to_be_used)
 
 
 # calling the function
 function_to_be_used()
+
+
+def decor1(func):
+    def inner():
+        x = func()
+        return x * x
+
+    return inner
+
+
+def decor(func):
+    def inner():
+        x = func()
+        return 2 * x
+
+    return inner
+
+
+@decor1
+@decor
+def num():
+    return 10
+
+
+print(num())
+
+
+def hello_decorator(func):
+    def inner1(*args, **kwargs):
+
+        print("before Execution")
+
+        # getting the returned value
+        returned_value = func(*args, **kwargs)
+        print("after Execution")
+
+        # returning the value to the original frame
+        return returned_value
+
+    return inner1
+
+
+# adding decorator to the function
+@hello_decorator
+def sum_two_numbers(a, b):
+    print("Inside the function")
+    return a + b
+
+
+a, b = 1, 2
+
+# getting the value through return of the function
+print("Sum =", sum_two_numbers(a, b))
