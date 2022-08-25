99673e307dc29e5347f9a8ff530954334aea6367
Awesome Programmer <fluffycatsavage@gmail.com>
2022-08-25 16:36:01 -0400

    fix the code up

--- code_school/decorator_examples.py
+++ code_school/decorator_examples.py
@@ -56,8 +56,34 @@ def num():
     return 10
 
 
-# 400 will be returned
 print(num())
 
-# <class 'function'>
-print(type(num))
+
+def hello_decorator(func):
+    def inner1(*args, **kwargs):
+
+        print("before Execution")
+
+        # getting the returned value
+        returned_value = func(*args, **kwargs)
+        print("after Execution")
+
+        # returning the value to the original frame
+        return returned_value
+
+    return inner1
+
+
+# adding decorator to the function
+@hello_decorator
+def sum_two_numbers(a, b):
+    print("Inside the function")
+    return a + b
+
+
+a, b = 1, 2
+
+# getting the value through return of the function
+print("Sum =", sum_two_numbers(a, b))
+
+print(type(hello_decorator))
