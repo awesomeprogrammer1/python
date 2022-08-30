d90ff547b68cda19f90914c2978f939b8bb2abc7
Awesome Programmer <fluffycatsavage@gmail.com>
2022-08-30 16:54:02 -0400

    finish the python homework

--- code_school/my_telegram_bot/bot.py
+++ code_school/my_telegram_bot/bot.py
@@ -1,4 +1,6 @@
 import telebot
+from datetime import date
+import time
 
 config = {
     "name": "FluffyAndy_Bot",
@@ -7,10 +9,20 @@ config = {
 
 bot = telebot.TeleBot(config["token"])
 
+
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if message.text == "Hello":
+    if message.text == "Hello" or "hello":
         bot.send_message(message.chat.id, "Hello, User.")
+    if message.text == "Date" or "date":
+        today = date.today()
+        bot.send_message(message.chat.id, today)
+    if message.text == "Time" or "time":
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        bot.send_message(message.chat.id, current_time)
+    if message.text == "How are you" or "how are you":
+        bot.send_message(message.chat.id, "I am very good, thank you.")
 
 
 bot.polling(non_stop=True, interval=0)
