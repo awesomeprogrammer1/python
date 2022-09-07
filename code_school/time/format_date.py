215148b2d67b4717d4b70a182f6917b75eb7619f
Awesome Programmer <fluffycatsavage@gmail.com>
2022-09-07 14:37:32 -0400

    tidy code up

--- code_school/time/format_date.py
+++ code_school/time/format_date.py
@@ -3,7 +3,7 @@ import time
 
 seconds = float(
     input(
-        "Get the UTC date and time at a certain amount of seconds past the epoch. Format Example: 3600.0. Note: 1 hour is 3600.0 seconds "
+        "Get the UTC date and time at a certain amount of seconds past the epoch. Format Example: 3600.0. Note: 1 hour is 3600.0 seconds and 1 day is 86400 seconds. 1 year is 31536000 seconds and 1 leap year is 31622400 seconds "
     )
 )
 named_tuple = time.localtime()
