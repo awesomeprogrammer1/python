30c185328389f01a60bbb266a3e068767b393efd
Awesome Programmer <fluffycatsavage@gmail.com>
2022-09-10 13:47:21 -0400

    update code

--- code_school/time/test_file.py
+++ code_school/time/test_file.py
@@ -3,12 +3,11 @@ import datetime
 date_var = input("Input a date in the yyyy-mm-dd format: ")
 
 
-def validate(date_text):
+def validate(date_text: str) -> bool:
     try:
         datetime.datetime.strptime(date_text, "%Y-%m-%d")
     except ValueError:
         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
-    print(date_text)
 
 
 validate(date_var)
