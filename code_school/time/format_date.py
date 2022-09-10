415bd07519a28f62281d62193a98b68bf768765e
Awesome Programmer <fluffycatsavage@gmail.com>
2022-09-10 12:24:59 -0400

    update code to potentially fix problem

--- code_school/time/format_date.py
+++ code_school/time/format_date.py
@@ -1,26 +1,19 @@
 from datetime import date
-from multiprocessing.sharedctypes import Value
 import time
 from datetime import datetime
 
-seconds = input("Input in the format: 21 June, 2018 2:35 UTC: ")
-named_tuple = time.localtime()
 
-
-def format_date(func):
+def formatted_date(func):
     def inner(date_time):
-        try:
-            print("I am going to format the date")
-            date_object = datetime.strptime(date_time, "%d %B, %Y %H:%M UTC")
-            print(date_object)
-        except ValueError:
-            print("Error, invalid format. You entered", func(date_time))
+        print("I am going to format the date")
+        print(time.strftime("%Y-%m-%d, %I:%M %p UTC", time.gmtime(date_time)))
+
     return inner
 
 
-@format_date
-def get_date(date_time: str):
+@formatted_date
+def format_date(date_time):
     print(date_time)
 
 
-get_date(seconds)
+format_date(3600)
