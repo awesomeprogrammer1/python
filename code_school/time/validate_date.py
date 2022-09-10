e7c415db019cb048cf5cc2836e46f3d2732b1788
Awesome Programmer <fluffycatsavage@gmail.com>
2022-09-10 13:53:09 -0400

    finish assignment

--- code_school/time/validate_date.py
+++ code_school/time/validate_date.py
@@ -1,12 +1,14 @@
 import datetime
 
 
-def validate(date_text: str) -> bool:
+def is_valid_date(date_text: str) -> bool:
     try:
         datetime.datetime.strptime(date_text, "%Y-%m-%d")
+        return True
     except ValueError:
-        raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+        return False
+
 
 if __name__ == "__main__":
     date_var = input("Input a date in the yyyy-mm-dd format: ")
-    validate(date_var)
+    is_valid_date(date_var)
