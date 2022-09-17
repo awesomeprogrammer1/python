c9743c2dc6693a1fbf2c64760934efedbdb171bc
Awesome Programmer <fluffycatsavage@gmail.com>
2022-09-17 10:02:31 -0400

    finish up assignment

--- code_school/time/validate_date_test.py
+++ code_school/time/validate_date_test.py
@@ -3,14 +3,15 @@ from validate_date import is_valid_date
 
 
 @pytest.mark.parametrize(
-    "date_str, expected_result",
+    "date_str, date_format_type, expected_result",
     [
-        ("2022-09-01", True),
-        ("2022-44-55", False),
-        ("hedgie", False),
-        ("2022-21-12", False),
-        ("2021-02-29", False),
+        ("2022-09-01", "%Y-%m-%d", True),
+        ("2022-44-55", "%Y-%m-%d", False),
+        ("hedgie", "%m/%d/%y", False),
+        ("2022-21-12", "%Y-%m-%d", False),
+        ("2021-02-29", "%Y-%d-%m", False),
+        ("1982-23-11", "", False),
     ],
 )
-def test_is_valid_date(date_str: str, expected_result: bool):
-    assert is_valid_date(date_str) == expected_result
+def test_is_valid_date(date_str: str, date_format_type: str, expected_result: bool):
+    assert is_valid_date(date_str, date_format_type) == expected_result
