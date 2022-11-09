2dd2925135507945dd9ec7729f8add40a522cb4d
Awesome Programmer <fluffycatsavage@gmail.com>
2022-11-09 13:50:18 -0500

    finish assignment from hw

--- code_school/files/work_files/email-assignments/email5_assignment4_simple.py
+++ code_school/files/work_files/email-assignments/email5_assignment4_simple.py
@@ -10,13 +10,13 @@ file2_path = folder_with_info / "email5_assignment4_text2.txt"
 file1_handle = open(file1_path, "r")
 file2_handle = open(file2_path, "r")
 file1_words = file1_handle.read().split()
-file2 = file2_handle.read()
+file2 = file2_handle.read().lower().split()
 file1_handle.close()
 file2_handle.close()
 output_list = []
 for word in file1_words:
-    if word in file2:
-        output_list.append(word)
+    if word.lower() in file2:
+        output_list.append(word.lower())
 output_list_set = set(output_list)
 common_words = "\n".join(output_list_set)
 folder_with_info = Path("code_school/files/work_files")
