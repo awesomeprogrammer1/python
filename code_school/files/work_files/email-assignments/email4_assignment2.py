99a67dee0acc4413259972b6bcd429e975203ee7
Awesome Programmer <fluffycatsavage@gmail.com>
2022-11-23 13:14:41 -0500

    further updates to code

--- code_school/files/work_files/email-assignments/email4_assignment2.py
+++ code_school/files/work_files/email-assignments/email4_assignment2.py
@@ -38,20 +38,22 @@ while True:
         ["Add Artist", "Add Song", "Delete Artist", "Delete Song", "Edit Song", "Save Songs", "Load Songs", "Exit"],
     )
     if interface == "Add Artist":
+        artist_db = load_db()
         artist_name = enterbox(
             "Please enter the name of a musical artist", "Add Artist"
         )
         # artist_db = {"Andrew": {"Help":100, "Me": 132567}}
         # add_artist = "Andrew"
-        if artist_name in load_db():
+        if artist_name in artist_db:
             textbox("Error: Artist already exists")
         else:
-            load_db()[artist_name] = {}
+            artist_db[artist_name] = {}
         # artist_db = {"Andrew": {}}
-        save_db(load_db())
+        save_db(artist_db)
     elif interface == "Add Song":
+        artist_db = load_db()
         artist_name = enterbox("Please enter the name of a musical artist", "Add Song")
-        if artist_name not in load_db():
+        if artist_name not in artist_db:
             textbox("Error: Artist does not exist")
         else:
             song_info = multenterbox(
@@ -61,46 +63,51 @@ while True:
             )
             current_song_info = {}
             current_song_info[song_info[0]] = song_info[1]
-            load_db()[artist_name][song_info[0]] = song_info[1]
-            save_db(load_db())
+            artist_db[artist_name][song_info[0]] = song_info[1]
+            save_db(artist_db)
     elif interface == "Delete Artist":
+        artist_db = load_db()
         artist_name = enterbox("Please enter the name of the musical artist", "Delete Artist")
-        if artist_name not in load_db():
+        if artist_name not in artist_db:
             textbox("Error: Artist does not exist")
         else:
-            load_db().pop(artist_name)
-        save_db(load_db())
+            artist_db.pop(artist_name)
+        save_db(artist_db)
     elif interface == "Delete Song":
+        artist_db = load_db()
         artist_name = enterbox("Please enter the name of a musical artist", "Delete Song")
-        if artist_name not in load_db():
+        if artist_name not in artist_db:
             textbox("Error: Artist does not exist.")
         else:
             song_name = enterbox("Enter the name of the song you would like to delete", "Delete Song")
-            if song_name not in load_db()[artist_name]:
+            if song_name not in artist_db[artist_name]:
                 textbox("Error: Song does not exist.")
             else:
-                load_db()[artist_name].pop(song_name)
-                save_db(load_db())
+                artist_db[artist_name].pop(song_name)
+                save_db(artist_db)
     elif interface == "Edit Song":
+        artist_db = load_db()
         song_info = multenterbox(
                 "Enter Song Information",
                 "Edit Song",
                 ["Artist Name", "Song Name"],
             )
-        if song_info[0] not in load_db():
+        if song_info[0] not in artist_db:
             textbox("Error, artist doesnt exist.")
         else:
-            if song_info[1] not in load_db()[song_info[0]]:
+            if song_info[1] not in artist_db[song_info[0]]:
                 textbox("Error, song doesnt exist.")
             else:
                 edit_plays = enterbox("Please enter the new number of plays", "Edit Song Plays")
-                load_db()[song_info[0]][song_info[1]] = edit_plays
-                save_db(load_db())
+                artist_db[song_info[0]][song_info[1]] = edit_plays
+                save_db(artist_db)
     elif interface == "Save Songs":
-        save_db(load_db())
-        textbox(load_db(), "Save Songs", "Successfully saved")
+        artist_db = load_db()
+        save_db(artist_db)
+        textbox(artist_db, "Save Songs", "Successfully saved")
     elif interface == "Load Songs":
-        textbox(load_db())
+        artist_db = load_db()
+        textbox(artist_db)
     if interface == "Exit":
         break
 
