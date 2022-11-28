caa1daca6c29475286822a44964ae62cf5ae75e9
Awesome Programmer <fluffycatsavage@gmail.com>
2022-11-28 17:58:00 -0500

    change .txt in email5_assignment4_sets.py

--- code_school/files/email-assignments/email5_assignment4_sets.py
+++ code_school/files/email-assignments/email5_assignment4_sets.py
@@ -6,6 +6,7 @@ to the final file (each file must contain words)
 """
 from pathlib import Path
 
+
 def get_unique_words(file_name: str) -> set:
     folder_with_info = Path("code_school/files/work_files")
     file1_path = folder_with_info / file_name
@@ -13,22 +14,16 @@ def get_unique_words(file_name: str) -> set:
     file_words = file_read.read().split()
     file_read.close()
     words = set(file_words)
-
     return words
 
 
-
-base_set: set = set()
 given_file = input("Enter a file name ")
-given_file += ".txt"
-for word in get_unique_words(given_file):
-    base_set.add(word)
+base_set: set = get_unique_words(given_file)
 while True:
     given_file = input("Enter a file name or quit. to stop entering file names: ")
     if given_file == "quit.":
         break
     else:
-        given_file += ".txt"
         base_set.intersection_update(get_unique_words(given_file))
 print(base_set)
 words_in_all_files = "".join(base_set)
