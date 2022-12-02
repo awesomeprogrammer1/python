18ffe5f5104a54d121e77aca7e39d449fb7f4294
Awesome Programmer <fluffycatsavage@gmail.com>
2022-12-02 14:44:37 -0500

    attempt to fix authentication()

--- code_school/my_telegram_bot/bot.py
+++ code_school/my_telegram_bot/bot.py
@@ -19,10 +19,14 @@ current_time = time.strftime("%H:%M:%S", t)
 path = Path("code_school\\my_telegram_bot")
 user_db = path / "login_info.json"
 
+
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     if message.text.lower() == "start":
-        bot.send_message(message.chat.id, "Register or Authenticate yourself with 'register' and/or 'authenticate' to use commands")
+        bot.send_message(
+            message.chat.id,
+            "Register or Authenticate yourself with 'register' and/or 'authenticate' to use commands",
+        )
     elif message.text.lower() == "register":
         user_password = bot.send_message(message.chat.id, "Enter a password")
         bot.register_next_step_handler(user_password, registration)
@@ -30,7 +34,10 @@ def handle_text(message):
         authenticate_user = bot.send_message(message.chat.id, "Enter your password")
         bot.register_next_step_handler(authenticate_user, authentication)
         if authentication(message.chat.id) == False:
-            bot.send_message(message.chat.id, "Authentication failed, please enter 'start' to restart the process")
+            bot.send_message(
+                message.chat.id,
+                "Authentication failed, please enter 'start' to restart the process",
+            )
         else:
             if message.text.lower() == "hello":
                 bot.send_message(message.chat.id, "Hello, User.")
@@ -41,10 +48,14 @@ def handle_text(message):
             elif message.text.lower() == "how are you":
                 bot.send_message(message.chat.id, "I am very good, thank you.")
             elif message.text.lower() == "calculator":
-                action = bot.send_message(message.chat.id, "Enter operation")  # 2+ 2 -> 4
+                action = bot.send_message(
+                    message.chat.id, "Enter operation"
+                )  # 2+ 2 -> 4
                 bot.register_next_step_handler(action, calculator)
             elif message.text.lower() == "length":
-                action2 = bot.send_message(message.chat.id, "Enter any amount of characters")
+                action2 = bot.send_message(
+                    message.chat.id, "Enter any amount of characters"
+                )
                 bot.register_next_step_handler(action2, length)
             elif message.text.lower() == "count":
                 action3 = bot.send_message(message.chat.id, "Enter a sentence ")
@@ -52,8 +63,6 @@ def handle_text(message):
             elif message.text.lower() == "textinfo":
                 action4 = bot.send_message(message.chat.id, "Enter a piece of text")
                 bot.register_next_step_handler(action4, text_info)
-            elif message.text.lower() == "wsg bbg":
-                bot.send_message(message.chat.id, "bro wth 💀")
             else:
                 bot.send_message(
                     message.chat.id,
@@ -90,11 +99,33 @@ def length(message):
 def text_info(message):
     file1 = open(path1, "w")
     vowels = ["a", "e", "i", "o", "u"]
-    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+    consonants = [
+        "b",
+        "c",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
     counter_v = 0
     counter_c = 0
-    digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = [',', '.', '/', '!', '?']
+    digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = [",", ".", "/", "!", "?"]
     symbol_counter = 0
     number_counter = 0
     length_message = len(message.text.split())
@@ -145,17 +176,10 @@ def authentication(message):
     get_info = open(user_db, "r")
     get_user_info = json.load(get_info)
     get_info.close()
-    if message.text in get_user_info:
-        bot.send_message(message.chat.id, "Successfully Authenticated")
-        return True
-    else:
-        bot.send_message(message.chat.id, "Error: Authentication Unsuccessful")
-        return False
+    return message.text in get_user_info[message.chat.id]
+
 
 bot.polling(non_stop=True, interval=0)
-
-
-
 
 
 # text {word:count(word)}
