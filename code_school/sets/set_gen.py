66a979f62db5f6ad9508e1a433aa346630a735b1
Awesome Programmer <fluffycatsavage@gmail.com>
2022-12-28 15:48:54 -0500

    update code +1st test case pass

--- code_school/sets/set_gen.py
+++ code_school/sets/set_gen.py
@@ -1,4 +1,4 @@
-'''
+"""
 A list of natural numbers is provided. 
 It is required to form a set of them. 
 If any number is repeated, then convert it to a string according to the pattern: 
@@ -19,6 +19,28 @@ Result:
 {'5555555', 5, '55', '55555', '5555', '555555', '555'}
 {1, 2, 3, 5, 6, 7, '22', '2222', '22222', '222', '11', '222222'}
 
-'''
-def set_gen(list: list) -> set:
+"""
 
+
+def set_gen(list: list) -> set:
+    set_var = set(list)
+    base_set: set = set()
+    for number in set_var:
+        number_as_str = str(number)
+        count_num = list.count(number)
+        if count_num > 1:
+            if count_num <= 2:
+                number_as_str += number_as_str
+                base_set.add(number_as_str)
+            else:
+                new_num_as_str = number_as_str + number_as_str
+                base_set.add(new_num_as_str)
+                for i in range(count_num-2):
+                    new_num_as_str += number_as_str
+                    base_set.add(new_num_as_str)
+    set_var.symmetric_difference_update(base_set)
+    return set_var
+
+
+list_1 = [1, 1, 3, 3, 1]
+print(set_gen(list_1))
