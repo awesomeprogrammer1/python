6ad154419aed5059440c20577a3587868d3d3007
Awesome Programmer <fluffycatsavage@gmail.com>
2023-02-01 07:22:53 -0500

    UPDATE CLASSWORK

--- code_school/sort_assignments/implement_bubble_sort.py
+++ code_school/sort_assignments/implement_bubble_sort.py
@@ -1,12 +1,19 @@
-list_var = [34, 19, 96, 2, 43, 64]
+list_var = [34, 19, 96, 2, 43, 64, 95, 75, 23]
 
 
-def BubbleSort(list):
-    for i in range(len(list) - 1):
-        for j in range(len(list) - 1):
-            if list[j] > list[j + 1]:
-                list[j], list[j + 1] = list[j + 1], list[j]
-    return list
+def BubbleSort(lst):
+    myList = list(lst)
+    count = 0
+    for i in range(len(myList) - 1):
+        for j in range(len(myList) - i - 1):
+            count += 1
+            if myList[j] > myList[j + 1]:
+                myList[j], myList[j + 1] = myList[j + 1], myList[j]
+
+    print(count)
+    return myList
 
 
+print(list_var)
 print(BubbleSort(list_var))
+print(list_var)
