ee1c7305e75fac339498c86d3780d7e903976fd9
Awesome Programmer <fluffycatsavage@gmail.com>
2023-02-21 15:24:24 -0500

    finish assignment

--- code_school/dictionaries/define_dict.py
+++ code_school/dictionaries/define_dict.py
@@ -1,7 +1,7 @@
 dict_var = {}
 
 
-value_of_a = 8
+# value_of_a = 8
 
 # if "a" in dict_var.keys():
 #     dict_var["a"] = value_of_a
@@ -16,18 +16,30 @@ value_of_a = 8
 #     value_of_a = 1
 
 
-print(value_of_a)
+# print(value_of_a)
+
+"""
+Based on the provided passage of text, determine the 3 most common characters in it.
+Spaces should be ignored (not taken into account when counting).
+To display the results of calculations, you need to write the top3(st) function.
+Present the result of the function as a string: "symbol - number of times, symbol - number of times ...".
+
+"""
+
 
 def top3(st: str) -> str:
     symbol_count: dict = {}
-    value_of_symbol = 0
     for symbol in st:
-        if symbol in symbol_count.keys():
-            value_of_symbol += 1
-        if symbol != " ":
-            symbol_count[symbol] = value_of_symbol
-    print(symbol_count)
-    return "e"
+        if symbol == " ":
+            continue
+        if symbol not in symbol_count:
+            symbol_count[symbol] = 1
+        else:
+            symbol_count[symbol] += 1
+    ordered_symbols = sorted(symbol_count.items(), key=lambda x:x[1], reverse=True)
+    top3_symbols = ordered_symbols[:3]
+    return f"Symbol: {top3_symbols[0][0]} | Number of occurences: {top3_symbols[0][1]}. Symbol: {top3_symbols[1][0]} | Number of occurences: {top3_symbols[1][1]}. Symbol: {top3_symbols[2][0]} | Number of occurences: {top3_symbols[2][1]}"
+    
 
 print(top3("col caun lreo"))
 
