85547f126ee8adf98e4325c06bf58de6c060203e
awesomeprogrammer1 <66175906+awesomeprogrammer1@users.noreply.github.com>
2023-03-17 15:09:45 -0400

    Update code_school/lists/number_pairs.py
    simplify code
    
    Co-authored-by: Sergei Ryabkov <sryabkov@users.noreply.github.com>

--- code_school/lists/number_pairs.py
+++ code_school/lists/number_pairs.py
@@ -7,7 +7,4 @@ def number_pairs(lst: list) -> list:
         for i in range(len(lst)-1):
             if lst[i] >= 0 and lst[i+1] >= 0 or lst[i] < 0 and lst[i+1] < 0:
                 return [lst[i], lst[i+1]]
-        else:
-            return []
-    else:
-        return []
+    return []
