a85c2bb45143ff8e81e1c97e857250ebb4ca3800
Awesome Programmer <fluffycatsavage@gmail.com>
2023-04-02 11:48:29 -0400

    update sequence_of_int_backup

--- code_school/recursion/sequence_of_int_backup.py
+++ code_school/recursion/sequence_of_int_backup.py
@@ -3,6 +3,14 @@
 "Return the same list in reverse order, using recursion"
 
 
-def sequence_of_int(lst: list) -> list:
-    return []
+
+def sequence_of_int(lst: list[int]) -> list:
+    current_elem = lst.pop(lst.index(0)-1)
+    lst.append(current_elem)
+    if 0 == lst[0]:
+        return lst
+    else:
+        return sequence_of_int(lst)
+    
+print(sequence_of_int([4,8,9,0]))
 
