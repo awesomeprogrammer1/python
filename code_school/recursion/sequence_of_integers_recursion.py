81dcae0bc32d81692aab62a609e9c4d35066cfb8
Awesome Programmer <fluffycatsavage@gmail.com>
2023-04-07 10:09:10 -0400

    update format

--- code_school/recursion/sequence_of_integers_recursion.py
+++ code_school/recursion/sequence_of_integers_recursion.py
@@ -5,17 +5,17 @@ Determine what is the largest number of consecutive elements of this sequence th
 '''
 
 
-def sequence_of_integers(lst: list) -> int:
-    counter = 1
-    longest_sequence = 1
-    for i in range(len(lst)-1):
-        if lst[i] == lst[i+1]:
-            counter += 1
-        else:
-            if counter > longest_sequence:
-                longest_sequence = counter
-            counter = 1
-    return longest_sequence
+# def sequence_of_integers(lst: list) -> int:
+#     counter = 1
+#     longest_sequence = 1
+#     for i in range(len(lst)-1):
+#         if lst[i] == lst[i+1]:
+#             counter += 1
+#         else:
+#             if counter > longest_sequence:
+#                 longest_sequence = counter
+#             counter = 1
+#     return longest_sequence
 
 
-print(sequence_of_integers([1, 2, 2, 2, 3, 3, 4, 0]))
+# print(sequence_of_integers([1, 2, 2, 2, 3, 3, 4, 0]))
