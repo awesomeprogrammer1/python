a486c325d34ddfb4bbb4703c3db6ccfe7824d53a
Awesome Programmer <fluffycatsavage@gmail.com>
2023-04-07 10:57:58 -0400

    update code

---
+++ code_school/recursion/max_index_in_list_test.py
@@ -0,0 +1,26 @@
+import pytest
+from max_index_in_list import max_index, __max_element
+
+
+@pytest.mark.parametrize(
+    "list_of_int, result",
+    [
+        ([1, 3, 4, 2, 0], 2),
+        ([4, 8, 9, 0, 7, 11, 4, 3, 11, 2], 5),
+        ([34, 72, 94, 26, 19, 8, 42, 87, 107], 8),
+    ],
+)
+def test_max_index(list_of_int: list, result: int):
+    assert max_index(list_of_int) == result
+
+
+@pytest.mark.parametrize(
+    "list_of_int, result",
+    [
+        ([1, 3, 4, 2, 0], 4),
+        ([4, 8, 9, 0, 7, 11, 4, 3, 11, 2], 11),
+        ([34, 72, 94, 26, 19, 8, 42, 87, 107], 107),
+    ],
+)
+def test_max_element(list_of_int: list, result: int):
+    assert __max_element(list_of_int) == result
