f25779247955f055725997a7f55fdce16d2053c9
Awesome Programmer <fluffycatsavage@gmail.com>
2023-04-28 14:55:59 -0400

    update code

--- code_school/index_assignments/number_spiral.py
+++ code_school/index_assignments/number_spiral.py
@@ -1,11 +1,22 @@
-'''
+"""
 Output a table of size
 n*n, filled with numbers from 1 to n^2 in a spiral
- coming out of the upper left corner and twisting clockwise, 
- as shown in the example (here n=5):
+coming out of the upper left corner and twisting clockwise, 
+as shown in the example (here n=5):
 
+1  2  3  4  5
+16 17 18 19 6
+15 24 25 20 7
+14 23 22 21 8
+13 12 11 10 9
 
-'''
+[1, 2, 3, 4, 5, 16, 17, 18, 19, 6, 15, 24, 25, 20, 7, 14, 23, 22, 21, 8, 13, 12, 11, 10, 9]
+"""
+
 
 def number_sprial(n: int) -> list:
-    return []
+    num_spiral: list = []
+    for i in range(1, n ^ 2 + 1):
+        num_spiral.append[i]
+    
+ 
--- code_school/index_assignments/number_spiral_test.py
+++ code_school/index_assignments/number_spiral_test.py
@@ -5,7 +5,7 @@ from number_spiral import number_sprial
 @pytest.mark.parametrize(
     "n, result",
     [
-        (5, [1 2 3 4 5 16 17 18 19 6 15 24 25 20 7 14 23 22 21 8 13 12 11 10 9]),
+        (5, [1, 2, 3, 4, 5, 16, 17, 18, 19, 6, 15, 24, 25, 20, 7, 14, 23, 22, 21, 8, 13, 12, 11, 10, 9]),
     ],
 )
 def test_number_sprial(n: int, result: list):
