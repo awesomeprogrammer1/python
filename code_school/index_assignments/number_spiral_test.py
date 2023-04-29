d7b19e941a5071519f359b909d8443bbc74cd3a0
Awesome Programmer <fluffycatsavage@gmail.com>
2023-04-29 07:16:25 -0400

    create tests

--- code_school/index_assignments/number_spiral_test.py
+++ code_school/index_assignments/number_spiral_test.py
@@ -1,12 +1,12 @@
-import pytest
-from number_spiral import number_spiral
+# import pytest
+# from number_spiral import number_spiral
 
-@pytest.mark.parametrize(
-    "n, result",
-    [
-        (5, '1 2 3 4 5\n 16 17 18 19 6\n 15 24 25 20 7\n 14 23 22 21 8\n 13 12 11 10 9'),
-        (3, '1 2 3 4\n      5\n9 8 7 6'),
-    ],
-)
-def test_sequence_of_numbers(n: int, result: str):
-    assert number_spiral(n) == result
+# @pytest.mark.parametrize(
+#     "n, result",
+#     [
+#         (5, '1 2 3 4 5\n 16 17 18 19 6\n 15 24 25 20 7\n 14 23 22 21 8\n 13 12 11 10 9'),
+#         (3, '1 2 3 4\n      5\n9 8 7 6'),
+#     ],
+# )
+# def test_sequence_of_numbers(n: int, result: str):
+#     assert number_spiral(n) == result
--- code_school/index_assignments/sum_of_squares_test.py
+++ code_school/index_assignments/sum_of_squares_test.py
@@ -1,2 +1,17 @@
 import pytest
 from sum_of_squares import sum_of_squares
+
+
+@pytest.mark.parametrize(
+    "lst_of_num, result",
+    [
+        ([1, -1], 2),
+        ([1, -3, 5, -6, -10, 13], 340),
+    ],
+)
+def test_sum_of_squares(lst_of_num: list, result: int):
+    assert sum_of_squares(lst_of_num) == result
+
+
+
+
